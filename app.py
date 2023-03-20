8d984599b5038dfe5a76b9b7526a79b9363ae24f
Renzo Wijngaarden <renzorenzo55@gmail.com>
2023-03-20 10:39:35 -0700

    Added data and containers

--- app.py
+++ app.py
@@ -1,60 +1,107 @@
-from dash import Dash, html, dcc, Input, Output
-import altair as alt
-from vega_datasets import data
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
 import pandas as pd
-import numpy as np
+import dash_table
+import os
 
+# Load data
+data1 = pd.read_csv("https://raw.githubusercontent.com/toUpperCase78/formula1-datasets/master/Formula1_2022season_calendar.csv")
+data2 = pd.read_csv("data/circuits.csv").rename(columns={"name": "Circuit Name"})
+df = pd.merge(data1, data2)
 
-# Setup app and layout/frontend
-app = Dash(__name__,  external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
-app.title = "RadioItems / Checklist comparison"
-server = app.server
+# Create app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
+app.title = "F1 Dash"
+app._favicon = (os.path.join('image', 'f1.ico'))
 
-app.layout = html.Div([
-    html.H1('RadioItems / Checklist comparison'),
-    html.Iframe(
-        id = 'iframe',
-        style={'border-width': '0', 
-        'width': '100%',
-        'height': '400px',
-        "top": "50%",
-        "left": "50%",}),
-    html.H3('Checklist'),
-    html.P('The user can visualize multiple options at the same time'),
-    dcc.Checklist(
-        id = 'check',
-        options = ['A', 'B', 'C'], 
-        value = ['A', 'B', 'C']),
-    html.H3('RadioItems'),
-    html.P('The user can select only one option (the options are mutually exclusive )'),
-    dcc.RadioItems(id = 'radio',
-        options = ['set1', 
-                   'set2', 
-                   'tableau10',
-                   'accent' ,
-                   'dark2'],
-        value =  'set1')])
+# Define layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1("The Formula 1 2022 Season, Visualised", style={'textAlign': 'center'})
+        ], width=12)
+    ]),
+    
+    dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(
+            id="table",
+            columns=[
+                {"name": "Column", "id": "Column"},
+                {"name": "Value", "id": "Value"},
+            ],
+            style_cell={
+                "textAlign": "center",
+                "padding": "5px",
+            },
+            style_table={
+                "width": "100%",
+                "height": "800px",
+                "overflowY": "auto",
+            },
+            fill_width=False,
+            cell_selectable=False
+        ),
+        ], width=4),
+        
+        dbc.Col([
+            dcc.Graph(
+            id="map",
+            figure={
+                "data": [
+                    {
+                        "type": "scattermapbox",
+                        "lat": df["lat"],
+                        "lon": df["lng"],
+                        "mode": "markers",
+                        "marker": {"size": 10},
+                        "hovertemplate": "%{text}<extra></extra>",
+                        "text": df["Circuit Name"],
+                    }
+                ],
+                "layout": {
+                    "mapbox": {
+                        "style": "carto-darkmatter",
+                        "center": {"lat": df["lat"].mean(), "lon": df["lng"].mean()},
+                        "zoom": 2,
+                    },
+                    "margin": {"l": 0, "r": 0, "t": 0, "b": 0},
+                    "height": 400,
+                },
+            },
+        ),
+        ], width=8)
+    ]),
+    
+])
 
-
-# Set up callbacks/backend
+# Define callback to update table
 @app.callback(
-    Output('iframe', 'srcDoc'),
-    Input('check', 'value'),
-    Input('radio', 'value')
+    dash.dependencies.Output("table", "data"),
+    dash.dependencies.Input("map", "clickData"),
 )
-def plot_altair(cat, color):
-  np.random.seed(42)
-  source = pd.DataFrame(np.cumsum(np.random.randn(100, 3), 0).round(2),
-                    columns=['A', 'B', 'C'], index=pd.RangeIndex(100, name='x'))
-  source = source.reset_index().melt('x', var_name='category', value_name='y')
-  source = source[source["category"].isin(cat)]
-  line = alt.Chart(source).mark_line(interpolate='basis').encode(
-  x='x:Q',
-  y='y:Q',
-  color='category:N').configure_range(
-  category={'scheme': color})
-  return line.to_html()
+def update_table(click_data):
+    if not click_data:
+        return []
+    point_index = click_data["points"][0]["pointNumber"]
+    row = df.iloc[point_index]
+    data = [
+        {"Column": "Round", "Value": row["Round"]},
+        {"Column": "Race Date", "Value": row["Race Date"]},
+        {"Column": "Grand Prix Name", "Value": row["GP Name"]},
+        {"Column": "Country", "Value": row["Country"]},
+        {"Column": "City", "Value": row["City"]},
+        {"Column": "Number of Laps", "Value": row["Number of Laps"]},
+        {"Column": "Circuit Length(km)", "Value": row["Circuit Length(km)"]},
+        {"Column": "Race Distance(km)", "Value": row["Race Distance(km)"]},
+        {"Column": "Turns", "Value": row["Turns"]},
+        {"Column": "DRS Zones", "Value": row["DRS Zones"]},
+        {"Column": "Year of First GP", "Value": row["First GP"]},
+    ]
+    return data
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
